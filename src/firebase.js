ff21f0e0dfa810c7fd4d544236a56362253500f5
Bek <65237794+itsbek@users.noreply.github.com>
2021-04-06 08:00:06 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBdKUSGe77y0bP-848Jf-56YkOT50WvXEg',
-  authDomain: 'wayhi-app.firebaseapp.com',
-  projectId: 'wayhi-app',
-  storageBucket: 'wayhi-app.appspot.com',
-  messagingSenderId: '303883341840',
-  appId: '1:303883341840:web:e40ed143893a2861ccba95',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
   measurementId: 'G-RZ6YTVPF4Q',
 };
 const firebaseApp = firebase.initializeApp(firebaseConfig);
